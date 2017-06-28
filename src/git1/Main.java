ff4ceaf6b1e3e399845c850892b2aee58f30300d
lihengyan <425257969@qq.com>
2017-06-29 01:30:18 +0800

    天猫

--- src/git1/Main.java
+++ src/git1/Main.java
@@ -2,7 +2,7 @@ package git1;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("--ko2017年6月29日 01:25:58jh------df-");
+		System.out.println("-2017年6月29日 01:30:00-ko2017年6月29日 01:25:58jh------df-");
 	}
 
 }
